2e3cfa7a51f8bf6d695d745d14067ca6096b6ec3
PayamBabaee <abolfazl.babaee@regit.cars>
2019-03-21 16:02:10 +0000

    app and test

--- app.js
+++ app.js
@@ -1,30 +1,97 @@
-export class GameOfLife {
-    constructor() {
-        this.gridSizeX = 50;
-        this.gridSizeY = 50;
-        this.startingLifeCells = 40;
+export class Grid {
+    constructor(gridSizeX, gridSizeY) {
+        this.gridSizeX = gridSizeX;
+        this.gridSizeY = gridSizeY;
         this.grid = [];
 
         this.initialiseGrid();
-        this.initialRandomLife();
     }
     initialiseGrid() {
         this.grid = new Array(this.gridSizeX).fill(0).map(
             () => new Array(this.gridSizeY).fill(0)
         );
     }
-    initialRandomLife() {
-        for (let i = 0; i <= this.startingLifeCells; i++) {
-            let xPos = this.getRandomInt(this.gridSizeX);
-            let yPos = this.getRandomInt(this.gridSizeY);
+    getCell(x, y) {
+        if (this.grid[x] !== undefined && this.grid[x][y] !== undefined) {
+            return this.grid[x][y];
+        }
 
-            while(this.grid[xPos][yPos]) {
-                xPos = this.getRandomInt(this.gridSizeX);
-                yPos = this.getRandomInt(this.gridSizeY);
+        throw new Error(`cell does not exists: [x] ${x} [y] ${y}`);
+    }
+    setCell(x, y, value) {
+        if (this.grid[x] !== undefined && this.grid[x][y] !== undefined) {
+            this.grid[x][y] = + value;
+
+            return ;
+        }
+
+        throw new Error('cell does not exists');
+    }
+    countNeighbours(x, y) {
+        let startX = (x - 1 < 0) ? x : x - 1;
+        let startY = (y - 1 < 0) ? y : y - 1;
+        let endX = (x + 1 < this.gridSizeX) ? x + 1 : x;
+        let endY = (y + 1 < this.gridSizeY) ? y + 1 : y;
+
+        let neighbourCount = 0;
+        for (let row = startX; row <= endX; row++) {
+            for (let column = startY; column <= endY; column++) {
+                if ((row !== x || column !== y) && this.getCell(row, column)) {
+                    neighbourCount++;
+                }
+            }
+        }
+
+        return neighbourCount;
+    }
+    render() {
+        for (let row = 0; row < this.gridSizeX; row++) {
+            console.log(this.grid[row].join(' '));
+        }
+    }
+}
+
+export class GameOfLife {
+    constructor(grid) {
+        this.grid = grid;
+    }
+    addRandomLife(limit) {
+        for (let i = 0; i < limit; i++) {
+            let xPos = this.getRandomInt(this.grid.gridSizeX);
+            let yPos = this.getRandomInt(this.grid.gridSizeY);
+
+            while(this.grid.getCell(xPos, yPos)) {
+                xPos = this.getRandomInt(this.grid.gridSizeX);
+                yPos = this.getRandomInt(this.grid.gridSizeY);
             }
 
-            this.grid[xPos][yPos] = 1;
+            this.grid.setCell(xPos, yPos,1);
         }
+    }
+    getNextState(x, y) {
+        const aliveNeighbors = this.grid.countNeighbours(x, y);
+
+        return aliveNeighbors === 3 || aliveNeighbors === 2 && this.grid.getCell(x, y);
+    }
+    runStep() {
+        const nextGrid = new Grid(this.grid.gridSizeX, this.grid.gridSizeY);
+
+        for (let row = 0; row < this.grid.gridSizeX; row++) {
+            for (let column = 0; column < this.grid.gridSizeY; column++) {
+                nextGrid.setCell(row, column, this.getNextState(row, column));
+            }
+        }
+
+        return nextGrid;
+    }
+    start() {
+        console.clear();
+        let timerId = setInterval(() => {
+            let nextGrid = this.runStep();
+            this.grid = nextGrid;
+            console.clear();
+            console.log(this.grid.render());
+        }, 500);
     }
     getRandomInt(max) {
         return Math.floor(Math.random() * max);
--- app_test.js
+++ app_test.js
@@ -1,32 +1,89 @@
 import { assert } from 'chai';
-import { GameOfLife } from './app';
+import { GameOfLife, Grid } from './app';
 
-describe('Game of Life', function() {
-    describe('Has initialised default values', function() {
-        it('Grid width and height specified', function() {
-            const game = new GameOfLife();
+describe('Grid', function () {
+    describe('State', function () {
 
-            assert.exists(game.gridSizeX, 'GridSizeX has been defined');
-            assert.exists(game.gridSizeY, 'GridSizeY has been defined');
+        it('Grid width and height are initialised', function() {
+            const grid = new Grid(50, 50);
+
+            assert.exists(grid.gridSizeX, 'GridSizeX has been defined');
+            assert.exists(grid.gridSizeY, 'GridSizeY has been defined');
         });
+
         it('Grid matrix initialised', function () {
-            const game = new GameOfLife();
-            const expectedSize = game.gridSizeX * game.gridSizeY;
-            const actualSize = game.grid.reduce((sum, r) => sum + r.length, 0);
+            const grid = new Grid(50, 50);
+            const expectedSize = grid.gridSizeX * grid.gridSizeY;
+            const actualSize = grid.grid.reduce((sum, r) => sum + r.length, 0);
 
-            assert.exists(game.grid, 'Grid has been defined');
-            assert.equal(expectedSize, actualSize,  'Grid matrix has filled with empty values');
+            assert.equal(actualSize, expectedSize, 'Grid matrix has filled with empty values');
         });
-        it('random life initialised', function () {
-            const game = new GameOfLife();
-            const actualSize = game.grid.reduce((sum, r) => sum + r.reduce((sum, c) => sum + c), 0);
 
-            assert.isAtLeast(actualSize, game.startingLifeCells, 'Grid has enough life cell initialised');
-        });
     });
-    describe('Is calculating game state', function () {
-        it('Looking in to neighbour cells', function () {
+    describe('Functionality', function () {
+        it('Can read a cell value', function () {
+            const grid = new Grid(50, 50);
+            let cellValue = grid.getCell(1, 2);
 
+            assert.equal(cellValue, 0, 'getCell method return valid value');
+        });
+        it('Can set a cell value', function () {
+            const grid = new Grid(50, 50);
+            grid.setCell(1,2, 1);
+            grid.setCell(0,0, 1);
+
+            assert.exists(grid.setCell, 'setCell method exists');
+            assert.equal(grid.getCell(1, 2), 1, 'value of cell has been set correctly');
+            assert.equal(grid.getCell(0, 0), 1, 'value of cell has been set correctly');
+            assert.equal(grid.getCell(2, 2), 0, 'value of cell has been set correctly');
+        });
+        it('Counting neighbours', function () {
+            const grid = new Grid(4, 4);
+            grid.setCell(0,0, 1);
+            grid.setCell(0,2, 1);
+            grid.setCell(2,0, 1);
+
+            assert.equal(grid.countNeighbours(1, 1), 3, 'neighbours count is correct');
+            assert.equal(grid.countNeighbours(2, 1), 1, 'neighbours count is correct');
         })
     })
+});
+
+describe('Game of Life', function() {
+    describe('State ', function() {
+        it('random life initialised', function () {
+            const grid = new Grid(50, 50);
+            const game = new GameOfLife(grid);
+            game.addRandomLife(20);
+
+            const actualSize = game.grid.grid.reduce((sum, r) => sum + r.reduce((sum, c) => sum + c), 0);
+
+            assert.isAtLeast(actualSize, 20, 'Grid has enough life cell initialised');
+        });
+    });
+    describe('Functionality', function () {
+        it('calculate next state for a cell', function () {
+            const grid = new Grid(4, 4);
+            const game = new GameOfLife(grid);
+
+            game.grid.setCell(0,0, 1);
+            game.grid.setCell(0,2, 1);
+            game.grid.setCell(2,0, 1);
+
+            assert.equal(1, game.getNextState(1, 1));
+            assert.equal(0, game.getNextState(2, 0));
+        });
+    });
+});
+
+describe('t', function () {
+    it('calculate a step in evolution', function () {
+        const grid = new Grid(10, 10);
+        const game = new GameOfLife(grid);
+        game.addRandomLife(30);
+
+        game.start();
+
+        assert.equal(1, 1);
+    });
 });
